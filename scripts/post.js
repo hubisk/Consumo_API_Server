66a77dc00aa37b3c0f35b82ea6435ae15549106b
hubisk <rubenskisonas@gmail.com>
2024-01-23 21:09:29 -0300

    Aula Consumo API 23/01/2024

--- scripts/post.js
+++ scripts/post.js
@@ -1,10 +1,12 @@
 function validarParametros() {
-    const validarNomeSerie = document.getElementById("nomeSerie").value
-    const validartemporadas = document.getElementById("temporadas").value
-    const validarProdutora = document.getElementById("produtora").value
-    const validarAnoLancamento = document.getElementById("anoLancamento").value
+    const validarNomeSerie = document.getElementById("nomeSerie").value != '';
+    const validartemporadas = document.getElementById("temporadas").value != '';
+    const validarProdutora = document.getElementById("produtora").value != '';
+    const validarAnoLancamento = document.getElementById("anoLancamento").value != '';
 
-    if (!validarNomeSerie || !validartemporadas || !validarProdutora || !validarAnoLancamento) {
+    const botaoCadastroValidar = document.getElementById('btnCadastrar')
+
+    if (validarNomeSerie || validartemporadas |validarProdutora || validarAnoLancamento) {
         document.getElementById("btnCadastrar").removeAttribute("disabled")
     } else {
         alert("Todos os campos devem ser preenchidos!")
@@ -14,14 +16,14 @@ function validarParametros() {
 document.addEventListener("input", () => {
     validarParametros()
 })
-// Realizar méto POST p/ cadastrar uma nova série
+// Realizar método POST p/ cadastrar uma nova série
 
 document.getElementById("btnCadastrar").addEventListener("click", async (e) => {
     e.preventDefault()
     // Responsável por chamar a API
 
     // Endpoint da API
-    const url = "https://json-server-vercel-seven-neon.vercel.app/series"
+    const url = "https://json-server-vercel-git-main-hubisks-projects.vercel.app/series"
 
     // Capturar os valores do formulario
     const dadosEnviados = {
@@ -31,18 +33,6 @@ document.getElementById("btnCadastrar").addEventListener("click", async (e) => {
         "anoLancamento": document.getElementById("anoLancamento").value,
         "produtora": document.getElementById("produtora").value
     }
-
-    // document.getElementById("btnCadastrar").disabled = true;
-    // elements.dadosEnviados.addEventListener("input", function (liberado) {
-    //     let camposPreenchidos = elements.dadosEnviados.value;
-
-    //     if (elements.camposPreenchidos !==null && elements.camposPreenchidos !== "") {
-    //         document.getElementById("btnCadastrar").disabled = false;
-    //     } 
-    //     else {
-    //         document.getElementById("btnCadastrar").disabled = true;
-    //     }
-    // });
     
     try {
         await fetch(url, {
@@ -59,7 +49,12 @@ document.getElementById("btnCadastrar").addEventListener("click", async (e) => {
             alert("Série cadastrada com sucesso!")
         }
 
+        // remover o evento de click após cadastro
+        document.getElementById('btnCadastrar').removeEventListener('click', arguments.callee);
+        // se faz referencia para a propia função
+
         location.reload()
+
     } catch (error) {
         console.log(`O consumo via Post Não funcionou, ${error}`);
     }
--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,116 @@
 async function getSeries() {
+    const url = "https://json-server-vercel-git-main-hubisks-projects.vercel.app/series"
 
-    const url = "https://json-server-vercel-seven-neon.vercel.app/series"
-
-    const response = await fetch(url);
+    const response = await fetch(url)
     console.log(response);
 
-    const dados = await response.json();
+    const dados = await response.json()
     console.log(dados);
 
-    const listaSeries = document.getElementById("listaSeries")    
-    dados.forEach(series => {
-        
-        
-        const tr = document.createElement("tr");
-        const div =document.createElement("div")
-        
-        // criando células da tabela no HTML
+    const listaSeries = document.getElementById('listaSeries')
 
+    dados.forEach(series => {
+
+        const tr = document.createElement('tr');
+        const div = document.createElement('div')
+
+        //criar as células da nossa tabela
         const tdNomeSerie = document.createElement("td")
         tdNomeSerie.textContent = series.nomeSerie
 
-        const tdNumTemp = document.createElement("td")
+        const tdNumTemp = document.createElement('td')
         tdNumTemp.textContent = series.temporadas
 
-        const tdAnoLancamento = document.createElement("td")
+        const tdAnoLancamento = document.createElement('td')
         tdAnoLancamento.textContent = series.anoLancamento
 
-        const tdProdutora = document.createElement("td")
+        const tdProdutora = document.createElement('td')
         tdProdutora.textContent = series.produtora
 
-        const tdEdit = document.createElement("img")
-        tdEdit.src = "./assets/icons/pencil.svg"
-        
-        const tdDelete = document.createElement("img")
-        tdDelete.src = "./assets/icons/trash.svg"
+        const tdEdit = document.createElement('img')
+        tdEdit.src = './assets/icons/mdi_pencil.svg'
 
-        // adicionando o nosso td dentro do nosso tr
+        const btnCadastro = document.getElementById('btnCadastrar');
 
+        tdEdit.addEventListener('click', () => {
+
+            const id = series.id;
+
+            document.getElementById('nomeSerie').value = series.nomeSerie
+            document.getElementById('temporadas').value = series.temporadas
+            document.getElementById('produtora').value = series.produtora
+            document.getElementById('anoLancamento').value = series.anoLancamento
+
+            document.getElementById('title-form').textContent = 'Atualizar Série'
+            document.getElementById('btnCadastrar').textContent = 'Atualizar'
+
+            if (window.confirm("Você deseja realizar uma alteração na série? ")) {
+                //chamar a nossa API
+
+                btnCadastro.removeEventListener('click', atualizarSerie)
+
+                btnCadastro.addEventListener('click', atualizarSerie);
+
+                async function atualizarSerie() {
+                    try {
+                        const dadosEnviadosAtualizar = {
+                            "id": null,
+                            "nomeSerie": document.getElementById('nomeSerie').value,
+                            "temporadas": document.getElementById('temporadas').value,
+                            "produtora": document.getElementById('produtora').value,
+                            "anoLancamento": document.getElementById('anoLancamento').value
+                        }
+
+                        const retorno = await fetch(`${url}/${id}`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(dadosEnviadosAtualizar)
+                        })
+
+                        if (retorno.ok) {
+                            alert("Série foi atualizada com sucesso!")
+                        }else{
+                            console.log(`Não foi possível atualizar a série ${retorno.status}`);
+                        }
+
+                        location.reload()
+                    } catch (error) {
+                        console.log(error);
+                    }
+                }
+            }
+        })
+
+        const tdDelete = document.createElement('img')
+        tdDelete.src = './assets/icons/mdi_trash.svg'
+
+        tdDelete.addEventListener('click', async () => {
+            //variavel para receber o id da serie
+
+            const id = series.id
+
+            if (window.confirm(`Você deseja mesmo remover a série: ${series.nomeSerie}`)) {
+                //chamar a nossa API
+                try {
+                    const retorno = await fetch(`${url}/${id}`, {
+                        method: 'DELETE'
+                    })
+
+                    if (retorno.ok) {
+                        alert("A série foi removida com sucesso!")
+                    }
+
+                    location.reload()
+                } catch (error) {
+                    console.log(`deu ruim na requisição ${error}`);
+                }
+            }
+        });
+
+
+        //adicionando o nosso td dentro do nosso tr 
         tr.appendChild(tdNomeSerie)
         tr.appendChild(tdNumTemp)
         tr.appendChild(tdAnoLancamento)
